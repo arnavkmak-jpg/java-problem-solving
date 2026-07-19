02721ba4a261819aa5d42b984a006321958596ef
Arnav <arnavkmak@gmail.com>
2026-07-19 14:31:56 +0530

    password checker using while loop in java

---
+++ src/calculator.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1;
+        double num2;
+        double result=0;
+        char operator;
+
+        System.out.print("Enter the first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter the operator(+,-,*,/,^): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        num2 = scanner.nextDouble();
+
+        switch(operator){
+            case '+' -> result = num1+num2;
+            case '-' -> result = num1-num2;
+            case '*' -> result = num1*num2;
+            case '/' -> result = num1/num2;
+            case '^' -> result = Math.pow(num1,num2);
+            default -> System.out.println("Invalid operator");
+
+        }
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
