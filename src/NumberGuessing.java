a6eaa3182790d8dba5a5c569700d99142916c7cf
Arnav <arnavkmak@gmail.com>
2026-07-21 07:41:38 +0530

    Number guessing game using Java

---
+++ src/NumberGuessing.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessing {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int attempt = 0;
+        int num;
+
+        int random_number = random.nextInt(1,101);
+
+        System.out.println("Number Guessing Game");
+        System.out.println("Guess the number between 1-100:");
+
+        do{
+            System.out.print("choose the number: ");
+             num = scanner.nextInt();
+            attempt++;
+            if (num<random_number){
+                System.out.println("TOO LOW!");
+            }
+            else if (num>random_number){
+                System.out.println("TOO HIGH!");
+            }
+            System.out.println("Attempts Taken: "+attempt);
+
+        }while(num!=random_number);
+
+        System.out.println("You Win!");
+        System.out.println("Attempts Taken: "+attempt);
+
+    }
+}
+
+
